4d14c1780bb781678779d545967dba60ed11356d
kawsarbyherself <kawsar_shafi@yahoo.com>
2022-01-21 00:12:30 -0800

    bugs again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 module.exports = function (app) {
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../front-end/index.html"));
   });
 
   app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../front-end/exercise.html"));
   });
 
   app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "../front-end/stats.html"));
   });
 };
